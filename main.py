2251c4532b5e05669e7499190528907058986bb2
CKlaudijus <klaudijus.c@gmail.com>
2022-08-09 21:13:47 +0300

    sestas commit,tobulinu klase

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ class Automobilis:
         self.modelis = modelis
         self.kuro_tipas = kuro_tipas
         print('Sukurtas auto', metai, modelis, kuro_tipas)
+
+"""ENHANCING CLASS WITH DEFINITIONS"""
+    def automobilis_vaziuoja(self):
+        print("Važiuoja")
+
+    def automobilis_stovi(self):
+        print("Stovi")
+
+    def auto_degalu_pildymas(self):
+        print("Degalai įpilti")
