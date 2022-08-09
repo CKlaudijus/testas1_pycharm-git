acc2583b8c491d547f8d77ebac452c9498fde9ca
CKlaudijus <klaudijus.c@gmail.com>
2022-08-09 21:10:31 +0300

    ketvirtas commit, naujas projektas

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def addition(l):
-    """This function calculates sum of list"""
-    return sum(l)
-
-def divison(x, y):
-    """THIS FUNCTION DIVIDES TWO NUMBERS"""
-    return sum(x / y)
-
-def
+"""ERASING EVERYTHING AND DOING FROM SCRATCH"""
