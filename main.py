37776030d85b9ad02c35f64a76974de4aef717f8
CKlaudijus <klaudijus.c@gmail.com>
2022-08-09 21:12:01 +0300

    penktas commit, kuriame klase

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-"""ERASING EVERYTHING AND DOING FROM SCRATCH"""
+"""CREATING A CLASS AND DEFINING IT"""
+class Automobilis:
+    def __init__(self, metai, modelis, kuro_tipas):
+        self.metai = metai
+        self.modelis = modelis
+        self.kuro_tipas = kuro_tipas
+        print('Sukurtas auto', metai, modelis, kuro_tipas)
